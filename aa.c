b9b043560629740898092b6cd997e07a2e380826
AFMT11 <AFMT11@yahoo.com>
2022-08-27 16:25:16 +0200

    2.0.1

--- aa.c
+++ aa.c
@@ -5,5 +5,8 @@ int  Sum_V(int a, int b)
 }
 int  Div_V(int a, int b)
 {
-    return a/b;
+    if(b)
+        return a/b;
+    else
+        return -1;
 }
--- main.c
+++ main.c
@@ -7,10 +7,13 @@ int main()
 {
     int x, y, sum_values, div_values;
     x = 50;
-    y = 25;
+    y = 10;
     sum_values = Sum_V(x,y);
     div_values = Div_V(x,y);
     printf("sum of %d & %d = %d\n", x, y, sum_values);
-    printf("div of %d & %d = %d\n", x, y, div_values);
+    if(Div_V(x,y) == -1)
+        printf("can't divide by 0!!!\n");
+    else
+        printf("div of %d & %d = %d\n", x, y, div_values);
     return 0;
 }
